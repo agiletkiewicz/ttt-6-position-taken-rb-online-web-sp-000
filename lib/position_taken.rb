1ce1c6fab06a6843d96b039bcb8a315619d9cb97
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-10 22:38:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  !board[index] == (" " || "" || nil)
+  if !board[index] == (" " || "" || nil)
+    return
+  else return true 
 end
